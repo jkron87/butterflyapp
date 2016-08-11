90504e548802dbf392d6f5b2fb7a93d7b87d2b86
Jennifer Kron <jenniferkron@gmail.com>
2016-08-11 11:28:21 -0400

    put map script into the controller

--- app/scripts/controllers/clueview1.js
+++ app/scripts/controllers/clueview1.js
@@ -16,4 +16,29 @@ angular.module('butterflyappApp')
         oText.innerHTML = count -= 10;
     }, false);
 
+    var map;
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 43, lng: -83},
+        zoom: 13,
+        disableDefaultUI: true,
+        draggable: false,
+        disableDoubleClickZoom: true,
+        keyboardShortcuts: false,
+        overviewMapControl: false,
+        scrollwheel: false
+      });
+    var cityCircle = new google.maps.Circle({
+        strokeColor: '#98ddde',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#98ddde',
+        fillOpacity: 0.35,
+        map: map,
+        center: {lat: 43, lng: -83},
+        radius: 1200
+      });
+
+    }
+  initMap()
   });
