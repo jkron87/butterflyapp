56c3272846bf0a3e1ff77c5df3288cf137fc23f9
Travis Stumbo <tstumbo1@gmail.com>
2016-08-11 22:33:11 -0400

    fixed points on clues 2 and 3

--- app/scripts/controllers/clueview2.js
+++ app/scripts/controllers/clueview2.js
@@ -8,10 +8,19 @@
  * Controller of the butterflyappApp
  */
 angular.module('butterflyappApp')
-  .controller('Clueview2Ctrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('Clueview2Ctrl', function ($scope) {
+  	var oButton = document.getElementById("hint2");
+    var oText = document.getElementById("points2");
+    var count = 100;
+    oButton.addEventListener('click', function() {
+
+
+      	if (count>50) {
+          	oText.innerHTML = count -= 10;
+      		  } else {
+      			oText.innerHTML = count = 50;
+      		  }
+    }, false);
+
+
   });
--- app/scripts/controllers/winningview.js
+++ app/scripts/controllers/winningview.js
@@ -7,11 +7,3 @@
  * # WinningviewCtrl
  * Controller of the butterflyappApp
  */
-angular.module('butterflyappApp')
-  .controller('WinningviewCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
