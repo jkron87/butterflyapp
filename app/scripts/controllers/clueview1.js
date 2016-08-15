d4d3edad95e6700434007ab1bc8dad72f45f2c11
Jennifer Kron <jenniferkron@gmail.com>
2016-08-15 17:58:36 -0400

    added checkin modal

--- app/scripts/controllers/clueview1.js
+++ app/scripts/controllers/clueview1.js
@@ -1,16 +1,9 @@
-'use strict';
+ (function () {
+     'use strict';
 
-/**
- * @ngdoc function
- * @name butterflyappApp.controller:Clueview1Ctrl
- * @description
- * # Clueview1Ctrl
- * Controller of the butterflyappApp
- */
-angular.module('butterflyappApp')
-  .controller('Clueview1Ctrl', ['$scope', 'pointsService', function ($scope, pointsService) {
-  	$scope.pointsPoints;
+     angular.module('butterflyappApp')
+         .controller('Clueview1Ctrl', [ function () {
 
+         }]);
 
-  }]);
-
+ }());
